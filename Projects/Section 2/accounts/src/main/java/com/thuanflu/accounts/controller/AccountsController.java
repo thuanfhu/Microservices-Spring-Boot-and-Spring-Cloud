a580264b5b392ff28a1ec7da55fb0ceddb269d7b
Quách Phú Thuận <thuanmobile1111@gmail.com>
2025-06-02 16:28:49 +0700

    feat: Section 2 - Configuring H2 DB & YAML properties

---
+++ Projects/Section 2/accounts/src/main/java/com/thuanflu/accounts/controller/AccountsController.java
@@ -0,0 +1,15 @@
+package com.thuanflu.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountsController {
+
+    @GetMapping("/hello")
+    public String handleHello() {
+        return "Hello";
+    }
+}
